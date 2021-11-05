02ad7bae0b7ae91b3c43167619871f0471b67e25
VaneManzanelli <vanemanza1@gmail.com>
2021-11-05 18:10:43 -0300

    comenté el código x un error en session

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,7 +1,7 @@
-def importe_total(request):
+"""def importe_total(request):
     total=0
     #if request.user.is_authenticated:
     for key, value in request.session["carro"].items():
         total=total+float(value["precio"])
         
-    return {"importe_total":total}
+    return {"importe_total":total}"""
