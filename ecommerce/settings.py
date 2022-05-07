d8ede0a1c25d9c9971ad22036c230d11c9b7d5f9
vanesa <vanemanza1@gmail.com>
2022-05-07 20:48:41 -0300

    agrego variables de entorno

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gpfgxh5h$wjh*f*3qub0w_s8l9#%gi@_83_+_s2@)%spd6)3n^'
+
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
